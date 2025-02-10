ddf18e7d5b0021fd6ca364963a2d045e80e03168
Weerawut <weerawut@live.com>
2025-02-10 09:36:19 +0700

    Initial Commit

---
+++ CityTour/CityTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityTourApp.swift
+//  CityTour
+//
+//  Created by Weerawut Chaiyasomboon on 10/2/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct CityTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
